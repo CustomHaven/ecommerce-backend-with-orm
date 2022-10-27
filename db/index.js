05b24c299a3db6dcbbe63c4040aeb8b562ff24ec
CustomHaven <mmuseaden@gmail.com>
2022-10-27 02:08:44 +0100

    comit

--- db/index.js
+++ db/index.js
@@ -13,6 +13,10 @@ const process = require("process");
 // };
 
 let db;
+console.log("process.env.NODE_ENV");
+console.log(process.env.NODE_ENV);
+console.log("process.env.NODE_ENV");
+
 if (process.env.NODE_ENV === "production") {
   db = new Sequelize(DB.DB_URL, {
     logging: false,
