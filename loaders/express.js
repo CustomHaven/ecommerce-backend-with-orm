fa56a2a9973fa916b7f35e6466120422e72a88fa
CustomHaven <mmuseaden@gmail.com>
2023-07-22 03:56:20 +0100

    take 59

--- loaders/express.js
+++ loaders/express.js
@@ -1,4 +1,3 @@
-const corsMiddleware = require('../cors');
 const cors = require("cors");
 const morgan = require('morgan');
 // const session = require('express-session');
@@ -24,80 +23,22 @@ module.exports = (app, express) => {
 
   app.set('view engine', 'ejs');
 
-  // app.use(morgan("dev"));
+  const corsWhitelist = ["http://localhost:3000", "http://localhost:5000", FRONTEND];
+  const corsOptions = {
+    origin: (originReq, callback) => { // Access-Control-Allow-Origin
+      if (corsWhitelist.indexOf(originReq) !== -1) {
+        callback(null, { origin: true });
+      } else {
+        callback(null, { origin: false }); // disable CORS for this request
+      }
+    },
+    credentials: true, //Access-Control-Allow-Credentials: true
+    optionsSuccessStatus: 200,
+    allowedHeaders: ["Access-Control-Allow-Origin", "Access-Control-Allow-Headers", "Access-Control-Allow-Methods", "X-Requested-With", "Content-Type", "Accept", "Authorization"],
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS", "HEAD"],
+  };
 
-  // const corsWhitelist = ["http://localhost:3000", "http://localhost:5000", FRONTEND];
-
-  // const corsOptions = {
-  //   origin: (originNow, callback) => {
-  //     loggers.info("INSIDE CORSOPTIONS!!!! originNow!YES!!!!!");
-  //     loggers.info(originNow);
-  //     // loggers.info("INSIDE CORSOPTIONS!!!! originNow!!!!!");
-  //     // loggers.info(originNow.header());;
-
-  //     if (corsWhitelist.indexOf(originNow) !== -1) {
-  //       callback(null, true); // reflect (enable) the requested origin in the CORS response
-  //     } else {
-  //       callback(null, false); // disable CORS for this request
-  //     }
-  //   },
-  //   // origin: ["http://localhost:3000", "http://localhost:5000", FRONTEND],
-  //   credentials: true, //Access-Control-Allow-Credentials: true
-  //   optionSuccessStatus: 200,
-  //   allowHeaders: ["Access-Control-Allow-Origin", "Access-Control-Allow-Headers", "Access-Control-Allow-Methods", "X-Requested-With", "Content-Type", "Accept"],
-  //   methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
-  // };
-  // app.use(cors(corsOptions));
-
-  // app.options("*", cors(corsOptions), (req, res, next) => next());
-  // app.all("*", cors(corsOptions), (req, res, next) => next());
-  // app.use(cors());
-
-  // app.all('*', cors(corsOptions));
-
-  // app.all("*", cors(cookieOptions), (req, res, next) => {
-  //   loggers.info("inside app.all looking at res.header");
-  //   loggers.info(res.header);
-  //   loggers.info("inside app.all DONE!");
-
-  //   res.header("Access-Control-Allow-Origin", "*");
-  //   res.header("Access-Control-Allow-Headers", "X-Requested-With");
-  //   next()
-  // });
-
-  // app.use(bodyParser.json());
-  // app.use(bodyParser.urlencoded({ extended: true }));
-
-
-
-/* ///////////////////////////////////////////////////////////////////////////////////////////////////////// */
-
-
-// app.use((req, res, next) => {
-//   res.header('Access-Control-Allow-Origin', FRONTEND);
-//   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-//   res.header('Access-Control-Allow-Credentials', true);
-//   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH');
-//   next();
-// });
-
-
-
-/* /////////////////////////////////////////////////////////////////////////////////////////////////////// */
-
-  // app.options("*", corsMiddleware, (req, res, next) => {
-  //   loggers.info("OPTIONS GIVE ME PASSING IT");
-  //   next();
-  // });
-  // app.all("*", corsMiddleware, (req, res, next) => {
-  //   loggers.info("all routes hit!!!!!");
-  //   next();
-  // });
-  app.use(corsMiddleware);
-
-  // app.use(cors({
-  //   origin: FRONTEND
-  // }));
+  app.use(cors(corsOptions));
 
   app.use(express.json());
 
