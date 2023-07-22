d3a661e2fbe1015cc271b895d3ca841933beb708
CustomHaven <mmuseaden@gmail.com>
2023-07-22 03:21:35 +0100

    take 54

--- cors/index.js
+++ cors/index.js
@@ -5,19 +5,20 @@ const { FRONTEND } = require("../config");
 const corsWhitelist = ["http://localhost:3000", "http://localhost:5000", FRONTEND];
 
 const corsOptions = {
-    // origin: (originReq, callback) => { // Access-Control-Allow-Origin
-    //     loggers.info("INSIDE CORSOPTIONS!!!! originReq!YES!!!!!");
-    //     loggers.info(originReq); // the origin website name
-    //     if (corsWhitelist.indexOf(originReq) !== -1) {
-    //         // return callback(null, true); // reflect (enable) the requested origin in the CORS response
-    //     } else {
-    //         return callback(null, false); // disable CORS for this request
-    //     }
-    // },
+    origin: (originReq, callback) => { // Access-Control-Allow-Origin
+        loggers.info("INSIDE CORSOPTIONS!!!! originReq!YES!!!!!");
+        loggers.info(originReq); // the origin website name
+        if (corsWhitelist.indexOf(originReq) !== -1) {
+            callback(null, true);
+            // return callback(null, true); // reflect (enable) the requested origin in the CORS response
+        } else {
+            callback(null, false); // disable CORS for this request
+        }
+    },
     // origin: ["http://localhost:3000", FRONTEND],
-    origin: "https://custom-haven-ecommerce.vercel.app",
+    // origin: "https://custom-haven-ecommerce.vercel.app",
     credentials: true, //Access-Control-Allow-Credentials: true
-    optionSuccessStatus: 200,
+    optionsSuccessStatus: 200,
     allowHeaders: ["Access-Control-Allow-Origin", "Access-Control-Allow-Headers", "Access-Control-Allow-Methods", "X-Requested-With", "Content-Type", "Accept"],
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
 };
